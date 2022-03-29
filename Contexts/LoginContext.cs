25f4632ec0bcb35bfd9875a34f0d6e14a759327a
Biswajit Sahu <BiswajitS@edynamic.net>
2022-03-29 12:29:44 +0530

    file added

---
+++ Contexts/LoginContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginMicroservice.Models;
+
+namespace LoginMicroservice.Contexts
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options) : base(options)
+        { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Usertoken> Usertokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Usertoken>().ToTable("Usertoken");
+          
+        }
+             
+    }
+}
+
---
+++ Models/Usertoken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginMicroservice.Models
+{
+    public class Usertoken
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string Token { get; set; }
+    }
+}
